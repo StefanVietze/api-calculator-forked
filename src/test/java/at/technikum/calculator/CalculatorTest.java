e84c191884f32f9a1c8cd922ddac3116704c2e73
Stefan Vietze <stefan@vietze.info>
2022-06-15 19:57:10 +0200

    Add multiply feature

--- src/test/java/at/technikum/calculator/CalculatorTest.java
+++ src/test/java/at/technikum/calculator/CalculatorTest.java
@@ -1,15 +1,22 @@
 package at.technikum.calculator;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class CalculatorTest {
 
+    private Calculator calc;
+
+    @BeforeEach
+    void initialize() {
+        calc = new Calculator();
+    }
+
     @Test
     void sum() {
         // arrange
-        Calculator calc = new Calculator();
         int a = 5;
         int b = 10;
 
@@ -18,5 +25,18 @@ class CalculatorTest {
 
         // assert
         assertEquals(15, result);
+    }
+
+    @Test
+    void multiplicationTestNormalUsage() {
+        // arrange
+        int a = 2;
+        int b = 3;
+
+        // act
+        int result = calc.multiply(a, b);
+
+        // assert
+        assertEquals(6, result);
     }
 }
